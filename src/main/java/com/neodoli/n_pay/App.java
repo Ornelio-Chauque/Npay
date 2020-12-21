1c06a61241a8878edba9a75749582715899e75f4
Ornélio Chaúque <chauque.ornelio@gmail.com>
2020-12-21 11:21:18 +0200

    Project setup

---
+++ src/main/java/com/neodoli/n_pay/App.java
@@ -0,0 +1,13 @@
+package com.neodoli.n_pay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
